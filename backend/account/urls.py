86047ce27ffb72a3f15b64cc37e18bda35dff2b5
ASHWANI TRIPATHI <tashwani475@gmail.com>
2024-07-01 02:42:38 +0530

    login done

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,13 +1,11 @@
 # urls.py
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from account.views import LoginView
 from .views import signup
 
 urlpatterns = [
     # path('me/', api.me, name='me'),
     path('user/signup/', signup, name='signup'),
-    # path('login/', LoginView.as_view(), name='token_obtain'),
-    # path('login1/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('user/login/', TokenObtainPairView.as_view(), name='token_obtain'),
     # path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -20,29 +20,7 @@ def signup(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def get_tokens_for_user(user):
-    refresh = RefreshToken.for_user(user)
-    return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-    }
 
-class LoginView(APIView):
-    renderer_classes=[UserRenderer]
-    def post(self,request,format=None):
-        print(request.data)
-        serializer=loginSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            email=serializer.data.get('email')
-            password=serializer.data.get('password')
-            user=authenticate(email=email,password=password)
-            print(user)
-            if user is not None:
-                tokens=get_tokens_for_user(user)
-                return Response({'msg':'login success','tokens':tokens},status=status.HTTP_200_OK)
-            else :
-               return  Response({'erros':{'non_field_errors':['email or password is invalid']}},status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
     
 
